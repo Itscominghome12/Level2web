1d756cba6c15b355b72a1e2d9cdecc211268f4be
Itscominghome12 <Cp92@mahurangi.school.nz>
2021-07-26 09:04:09 +1200

    Basic
    Changed to basic format

---
+++ Nav/about.php
@@ -0,0 +1,70 @@
+<?php include("head_nav.html");?>
+
+       <div class="jumbotron text-center"> 
+ 
+           
+        
+           
+           
+</div>
+
+<div class="about-section">
+  <h1>About Canopy</h1>
+  <h5>Canopy Cancer Care is a leading private clinic for adult cancer patients across New zealand. Our team specialise in the care of patients requiring cancer treatment with chemotherapy, immunotherapy, antibody therapy, hormone therapy and more targeted therapies.</h5>
+  
+</div>
+
+<h1 style="text-align:center">Our Story</h1>
+<div class="row1">
+  <div class="column">
+    <div class="card">
+      <img src="Past.png" alt="Jane" style="width:100%">
+      <div class="container">
+        <h2>Past</h2>
+        <p>Canopy was founded in 2007 by John Doe. We started off as a small company with 10 staff operating within a small 2 room complex located at North Shore. We then began to develop over time and included some of the more enhanced technology to help deal with cancer. We mainly used to help patients with breast cancer however we then in 2011 opened a new clinic in Epsom Auckland that now deals with all different types of Cancers. </p>
+
+       
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="Presant.png" alt="Mike" style="width:100%">
+      <div class="container">
+        <h2>Present</h2>
+        <p>We currently have 4 fully functioning clinics located in Auckland, North shore, Tauranga and Whangarei. With a new clinic being built in Wellington. We have over 350 staff that have integrated into the canopy team and thrive in the latest technology that Canopy offers its patients. Each clinic deals with at least 30 patients a day that can range from chemotherapy treatment to help and advice.</p>
+     
+       
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+       <img class="future" src="Future.png">
+      <div class="container">
+        <h2>Future</h2>
+        <p>The Canopy vision is to offer New Zealanders world-class cancer care, delivered locally. Everyday and with every patient, we work towards this goal. We have built a strong team who are all passionate about our patients and the outcome that matters to them most. We aim to provide the highest quality of care and will coordinate all aspects of treatment where possible.</p>
+
+       
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<?php include("carousel.php");?>
+
+
+
+  
+       
+        
+        
+        
+        
+
+
+     
+</body>
---
+++ Nav/contact.php
@@ -0,0 +1,77 @@
+<?php include("head_nav.html");?>
+
+
+<div class="jumbotron text-center"> 
+ 
+           
+        
+           
+           
+</div>
+
+<div class="contact-top">
+  <h1>Contact</h1>
+  <h5>Use this page to contact the Canopy team</h5>
+  
+</div>
+
+<div class="container">
+  <form action="action_page.php">
+
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+    <label for="country">Country</label>
+    <select id="country" name="country">
+      <option value="australia">New Zealand</option>
+      <option value="canada">Australia</option>
+      <option value="usa">USA</option>
+    </select>
+
+    <label for="subject">Subject</label>
+    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+    <input type="submit" value="Submit">
+
+  </form>
+</div>
+
+
+ <div class="btn-group">
+    <button type="button" class="btn btn-primary">Phone numbers</button>
+    <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown">
+    </button>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Link 1</a>
+      <a class="dropdown-item" href="#">Link 2</a>
+    </div>
+  </div>
+  
+  <div class="btn-group">
+    <button type="button" class="btn btn-secondary">Other Canopy sites</button>
+    <button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown">
+    </button>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Link 1</a>
+      <a class="dropdown-item" href="#">Link 2</a>
+    </div>
+  </div>
+
+  <div class="btn-group">
+    <button type="button" class="btn btn-success">Doctors and Nurses</button>
+    <button type="button" class="btn btn-success dropdown-toggle dropdown-toggle-split" data-toggle="dropdown">
+    </button>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Link 1</a>
+      <a class="dropdown-item" href="#">Link 2</a>
+    </div>
+  </div>
+
+
+</body>
+
+
+
